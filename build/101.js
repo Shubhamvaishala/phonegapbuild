0cda6653e19c4f5cd7f8a11bed01e30b67e68e29
ypshukla <yogeshwar.shukla@vidyamantra.com>
2019-01-22 11:38:26 +0530

    v3.6.0 with logo update

--- build/101.js
+++ build/101.js
@@ -1,6 +1,6 @@
 webpackJsonp([101],{
 
-/***/ 1863:
+/***/ 1861:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- build/93.js
+++ build/93.js
@@ -1,6 +1,6 @@
 webpackJsonp([93],{
 
-/***/ 1872:
+/***/ 1870:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
