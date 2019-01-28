937613236df31112f7ff4970ec236458fffc9ded
ypshukla <yogeshwar.shukla@vidyamantra.com>
2019-01-28 10:01:00 +0530

    icon update

--- build/22.js
+++ build/22.js
@@ -1,6 +1,6 @@
 webpackJsonp([22],{
 
-/***/ 1867:
+/***/ 1865:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- build/43.js
+++ build/43.js
@@ -1,6 +1,6 @@
 webpackJsonp([43],{
 
-/***/ 1932:
+/***/ 1935:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
